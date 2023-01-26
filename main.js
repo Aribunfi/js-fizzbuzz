f69a0989916076645289ef15e7ba7aba4cbb05a8
Aribunfi <arianna@pabis.de>
2023-01-26 09:10:15 +0100

    numeri pari e dispari due colori

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ console.log("inizio ciclo");
 
 for (let i=1; i<=100; i++) {
 
-    if (i / 2 == 0) {
+    if (i % 2 == 0) {
         listEl.innerHTML += `<li class="box box-pari">${i}</li>`;
 }
     else {
@@ -18,6 +18,16 @@ for (let i=1; i<=100; i++) {
 console.log("fine ciclo");
 
 // al posto dei multipli di tre: "Fizz"
+
+const i = document.getElementById("list")
+
+if (i / 3 == 0) {
+    document.write("Fizz");
+}
+else if (i / 5 == 0) {
+    listEl.innerHTML += `<li class="box box-dispari">${i}</li>`;
+}
+
 
 // al posto dei multipli di cinque: "Buzz"
 
